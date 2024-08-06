6dfdceab199a1c5d2e0bc401494c5a93e936f574
rote1400 <rote1400@student.miun.se>
2024-08-06 22:41:00 +0200

    admin nav bar

--- ecommercestore/src/components/Nav.tsx
+++ ecommercestore/src/components/Nav.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Children, ComponentProps, ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 
 export function Nav({ children }: { children: ReactNode }) {
   return (
