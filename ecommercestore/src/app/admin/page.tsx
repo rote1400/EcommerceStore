088803414713974f3a63af95f2eab96ca49ed1cd
rote1400 <rote1400@student.miun.se>
2024-08-07 20:04:48 +0200

    sales data from db

--- ecommercestore/src/app/admin/page.tsx
+++ ecommercestore/src/app/admin/page.tsx
@@ -5,13 +5,27 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import prisma from "@/db/db";
 
-export default function AdminDashboard() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <DashboardCard title="Sales" subtitle="Test" body="body"/>
-    </div>
-  );
+async function getSalesData() {
+    const data = await prisma.order.aggregate({
+        _sum: { pricePaidInCents: true},
+        _count: true
+    })
+
+    return {
+        amount: (data._sum.pricePaidInCents || 0) / 100,
+        numberOfSales: data._count
+    }
+}
+
+export default async function AdminDashboard() {
+    const salesData = getSalesData()
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <DashboardCard title="Sales" subtitle="Test" body="body"/>
+        </div>
+    );
 }
 
 type DashboardCardProps = {
