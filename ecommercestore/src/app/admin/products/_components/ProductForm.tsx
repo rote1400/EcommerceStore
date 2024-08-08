f7a5ec1a3a9fab11389f48d3f5c0f2ba666e1f94
rote1400 <rote1400@student.miun.se>
2024-08-08 22:51:43 +0200

    error messages for fields in product form

--- ecommercestore/src/app/admin/products/_components/ProductForm.tsx
+++ ecommercestore/src/app/admin/products/_components/ProductForm.tsx
@@ -7,16 +7,18 @@ import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
 import { addProduct } from "../../_actions/products";
-import { useFormStatus } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 
 export function ProductForm() {
+  const [error, action] = useFormState(addProduct, {})
   const [priceInCents, setPriceInCents] = useState<number>();
 
   return (
-    <form action={addProduct} className="space-y-8">
+    <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
         <Input type="text" id="name" name="name" required />
+        {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price In Cents</Label>
@@ -31,18 +33,22 @@ export function ProductForm() {
         <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
         </div>
+        {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
         <Textarea id="description" name="description" required />
+        {error.description && <div className="text-destructive">{error.description}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
         <Input type="file" id="file" name="file" required />
+        {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
         <Input type="file" id="image" name="image" required />
+        {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
     </form>
