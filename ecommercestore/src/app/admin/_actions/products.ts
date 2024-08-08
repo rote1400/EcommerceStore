5af305a7b57994c52a3b0a69e33c202a24b3def8
rote1400 <rote1400@student.miun.se>
2024-08-08 14:02:37 +0200

    add schema, validations for new product

--- ecommercestore/src/app/admin/_actions/products.ts
+++ ecommercestore/src/app/admin/_actions/products.ts
@@ -1,5 +1,25 @@
-"use server"
+"use server";
+
+import { z } from "zod";
+
+const fileSchema = z.instanceof(File, { message: "Required" });
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/")
+);
+
+const addSchema = z.object({
+  name: z.string().min(1),
+  priceInCents: z.coerce.number().int().min(1),
+  description: z.string().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "Required"),
+  image: imageSchema.refine((file) => file.size > 0, "Required"),
+});
 
 export async function addProduct(formData: FormData) {
-   console.log(formData) 
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
 }
