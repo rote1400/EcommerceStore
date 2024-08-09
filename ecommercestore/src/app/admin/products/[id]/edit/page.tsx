c5212cb3f865f57a0eb39ddcadbbb8ae62c247a2
rote1400 <rote1400@student.miun.se>
2024-08-10 00:14:47 +0200

    edit a product, default values

---
+++ ecommercestore/src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,17 @@
+import prisma from "@/db/db";
+import { PageHeader } from "../../../_components/PageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+
+export default async function EditProductPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await prisma.product.findUnique({ where: { id } });
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm product={product} />
+    </>
+  );
+}
--- ecommercestore/src/app/admin/products/_components/ProductForm.tsx
+++ ecommercestore/src/app/admin/products/_components/ProductForm.tsx
@@ -8,16 +8,26 @@ import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
 import { addProduct } from "../../_actions/products";
 import { useFormState, useFormStatus } from "react-dom";
+import { Product } from "@prisma/client";
+import Image from "next/image";
 
-export function ProductForm() {
-  const [error, action] = useFormState(addProduct, {})
-  const [priceInCents, setPriceInCents] = useState<number>();
+export function ProductForm({ product }: { product?: Product | null }) {
+  const [error, action] = useFormState(addProduct, {});
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents
+  );
 
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -33,21 +43,41 @@ export function ProductForm() {
         <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
         </div>
-        {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
+        {error.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
-        {error.description && <div className="text-destructive">{error.description}</div>}
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description}
+        />
+        {error.description && (
+          <div className="text-destructive">{error.description}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product == null} />
+        {product != null && (
+          <div className="text-muted-foreground">{product.filePath}</div>
+        )}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required={product == null} />
+        {product != null && (
+          <Image
+            src={product.imagePath}
+            height="400"
+            width="400"
+            alt="Product Image"
+          />
+        )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
