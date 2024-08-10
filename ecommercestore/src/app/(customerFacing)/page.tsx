5e097cb395dfc27de05a6054cba4eabf8884c66e
rote1400 <rote1400@student.miun.se>
2024-08-11 00:41:44 +0200

    product card skeleton, 1 and 2 seconds wait for loading product cards

--- ecommercestore/src/app/(customerFacing)/page.tsx
+++ ecommercestore/src/app/(customerFacing)/page.tsx
@@ -1,11 +1,17 @@
-import { ProductCard } from "@/components/ProductCard";
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
 import { Button } from "@/components/ui/button";
 import prisma from "@/db/db";
 import { Product } from "@prisma/client";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
 
-function getMostPopularProducts() {
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+async function getMostPopularProducts() {
+  await wait(1000);
   return prisma.product.findMany({
     where: { isAvailableForPurchase: true },
     orderBy: { orders: { _count: "desc" } },
@@ -13,7 +19,8 @@ function getMostPopularProducts() {
   });
 }
 
-function getNewestProducts() {
+async function getNewestProducts() {
+  await wait(2000);
   return prisma.product.findMany({
     where: { isAvailableForPurchase: true },
     orderBy: { createdAt: "desc" },
@@ -38,7 +45,7 @@ type ProductGridSectionProps = {
   productsFetcher: () => Promise<Product[]>;
 };
 
-async function ProductGridSection({
+function ProductGridSection({
   productsFetcher,
   title,
 }: ProductGridSectionProps) {
@@ -54,10 +61,28 @@ async function ProductGridSection({
         </Button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {(await productsFetcher()).map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductSuspense productsFetcher={productsFetcher} />
+        </Suspense>
       </div>
     </div>
   );
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>;
+}) {
+  return (await productsFetcher()).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
 }
--- ecommercestore/src/components/ProductCard.tsx
+++ ecommercestore/src/components/ProductCard.tsx
@@ -12,12 +12,12 @@ import Link from "next/link";
 import Image from "next/image";
 
 type ProductCardProps = {
-    id: string
-    name: string
-    priceInCents: number
-    description: string
-    imagePath: string
-}
+  id: string;
+  name: string;
+  priceInCents: number;
+  description: string;
+  imagePath: string;
+};
 
 export function ProductCard({
   id,
@@ -27,15 +27,13 @@ export function ProductCard({
   imagePath,
 }: ProductCardProps) {
   return (
-    <Card className="flex overflow-hidden flex-col">
+    <Card className="overflow-hidden flex flex-col">
       <div className="relative w-full h-auto aspect-video">
         <Image src={imagePath} fill alt={name} />
       </div>
       <CardHeader>
         <CardTitle>{name}</CardTitle>
-        <CardDescription>
-          {formatCurrency(priceInCents / 100)}
-        </CardDescription>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
       </CardHeader>
       <CardContent className="flex-grow">
         <p className="line-clamp-4">{description}</p>
@@ -44,6 +42,30 @@ export function ProductCard({
         <Button asChild size={"lg"} className="w-full">
           <Link href={`/products/${id}/purchase`}>Purchase</Link>
         </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export function ProductCardSkeleton() {
+  return (
+    <Card className="overflow-hidden flex flex-col animate-pulse">
+      <div className="w-full aspect-video bg-gray-300" />
+      <CardHeader>
+        <CardTitle>
+          <div className="w-3/4 h-6 rounded-full bg-gray-300" />
+        </CardTitle>
+        <CardDescription>
+          <div className="w-1/2 h-4 rounded-full bg-gray-300" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="w-full h-4 rounded-full bg-gray-300" />
+        <div className="w-full h-4 rounded-full bg-gray-300" />
+        <div className="w-3/4 h-4 rounded-full bg-gray-300" />
+      </CardContent>
+      <CardFooter>
+        <Button disabled size={"lg"} className="w-full"></Button>
       </CardFooter>
     </Card>
   );
