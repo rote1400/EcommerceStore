c723eb2aedc5cadba0433c8ac8e974ef00e47b2a
rote1400 <rote1400@student.miun.se>
2024-08-11 12:13:30 +0200

    gitignore env

--- ecommercestore/src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ ecommercestore/src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -2,7 +2,7 @@ import prisma from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 export default async function PurchasePage({
   params: { id },
