17c37ad6e1779762cbe93c963bc8ffd3261e9422
rote1400 <rote1400@student.miun.se>
2024-08-11 15:13:47 +0200

    checkout form

---
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,8 @@
+type CheckoutFormProps = {
+  product: {};
+  clientSecret: string;
+};
+
+export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
+  return <h1>Form</h1>;
+}
--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
+import { CheckoutForm } from "./_components/CheckoutForm";
 
-const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY as string);
 
 export default async function PurchasePage({
   params: { id },
@@ -14,5 +15,20 @@ export default async function PurchasePage({
   });
   if (product == null) return notFound();
 
-  return <h1>Hi</h1>;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent");
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 }
