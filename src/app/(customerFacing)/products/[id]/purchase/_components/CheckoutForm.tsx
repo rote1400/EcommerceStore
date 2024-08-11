9354545142f687d15bcb3f73a4aa17e8c4fe9497
rote1400 <rote1400@student.miun.se>
2024-08-11 17:06:02 +0200

    product data in checkout form, name, price, description

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { formatCurrency } from "@/lib/formatters";
 import {
   Elements,
   PaymentElement,
@@ -7,9 +8,15 @@ import {
   useStripe,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
 
 type CheckoutFormProps = {
-  product: {};
+  product: {
+    name: string;
+    imagePath: string;
+    priceInCents: number;
+    description: string;
+  };
   clientSecret: string;
 };
 
@@ -19,44 +26,63 @@ const stripePromise = loadStripe(
 
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
   return (
-    <Elements
-      stripe={stripePromise}
-      options={{
-        clientSecret,
-        appearance: {
-          theme: "flat",
-          variables: {
-            colorPrimary: "#0570de",
-            colorBackground: "#f6f9fc",
-            colorText: "#30313d",
-            colorDanger: "#df1b41",
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <div className="flex gap-4 items-center">
+        <div className="flex-shrink-0 w-1/3 aspect-video relative">
+          <Image
+            className="object-cover"
+            src={product.imagePath}
+            fill
+            alt={product.name}
+          />
+        </div>
+        <div className="text-lg">
+          {formatCurrency(product.priceInCents / 100)}
+        </div>
+        <h1 className="text-2xl font-bold">{product.name}</h1>
+        <div className="line-clamp-3 text-muted-foreground">
+          {product.description}
+        </div>
+      </div>
+      <Elements
+        stripe={stripePromise}
+        options={{
+          clientSecret,
+          appearance: {
+            theme: "flat",
+            variables: {
+              colorPrimary: "#0570de",
+              colorBackground: "#f6f9fc",
+              colorText: "#30313d",
+              colorDanger: "#df1b41",
+            },
+            rules: {
+              ".Label": {
+                color: "#6b7c93",
+              },
+              ".Input": {
+                border: "1px solid #ccd0d2",
+                boxShadow: "none",
+              },
+              ".Tab": {
+                border: "none",
+              },
+              ".Tab:hover": {
+                backgroundColor: "#f8f9fa",
+              },
+              ".Tab--selected": {
+                borderColor: "#0570de",
+              },
+              ".PaymentMethod": {
+                boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",
+              },
+            },
           },
-          rules: {
-            ".Label": {
-              color: "#6b7c93",
-            },
-            ".Input": {
-              border: "1px solid #ccd0d2",
-              boxShadow: "none",
-            },
-            ".Tab": {
-              border: "none",
-            },
-            ".Tab:hover": {
-              backgroundColor: "#f8f9fa",
-            },
-            ".Tab--selected": {
-              borderColor: "#0570de",
-            },
-            ".PaymentMethod": {
-              boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",
-            },
-          },
-        },
-      }}
-    >
-      <Form />
-    </Elements>
+        }}
+      >
+        <Form />
+      </Elements>
+    </div>
   );
 }
 
