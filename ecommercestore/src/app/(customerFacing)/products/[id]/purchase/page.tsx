a0af635672ecd5ebd1da95b09e695c442010abce
rote1400 <rote1400@student.miun.se>
2024-08-11 12:05:43 +0200

    purchase a product

---
+++ ecommercestore/src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -0,0 +1,18 @@
+import prisma from "@/db/db";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function PurchasePage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  if (product == null) return notFound();
+
+  return <h1>Hi</h1>;
+}
