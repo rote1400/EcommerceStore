ded6d301224d6ab4dad0f86f04df0d28b7a60e1b
rote1400 <rote1400@student.miun.se>
2024-08-12 16:08:22 +0200

    download button product purchased

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
 import prisma from "@/db/db";
 import { formatCurrency } from "@/lib/formatters";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
 
@@ -22,10 +24,13 @@ export default async function SuccessPage({
   });
   if (product == null) return notFound();
 
-  const isSuccess = paymentIntent.status === "succeeded"
+  const isSuccess = paymentIntent.status === "succeeded";
 
   return (
     <div className="max-w-5xl w-full mx-auto space-y-8">
+      <h1 className="text-4xl font-bold">
+        {isSuccess ? "Success!" : "Error!"}
+      </h1>
       <div className="flex gap-4 items-center">
         <div className="flex-shrink-0 w-1/3 aspect-video relative">
           <Image
@@ -35,14 +40,33 @@ export default async function SuccessPage({
             alt={product.name}
           />
         </div>
-        <div className="text-lg">
-          {formatCurrency(product.priceInCents / 100)}
-        </div>
-        <h1 className="text-2xl font-bold">{product.name}</h1>
-        <div className="line-clamp-3 text-muted-foreground">
-          {product.description}
+        <div>
+          <div className="text-lg">
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+          <Button className="mt-4" size={"lg"} asChild>
+            {isSuccess ? (
+              <a
+                href={`/products/download/${createDownloadVerification(
+                  product.id
+                )}`}
+              ></a>
+            ) : (
+              <Link href={`/products/${product.id}/purchase`}>Try again</Link>
+            )}
+          </Button>
         </div>
       </div>
     </div>
   );
+}
+
+function createDownloadVerification(productId: string) {
+  return prisma.downloadVerification.create({
+    data: { productId, expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24) },
+  });
 }
