f5b1fb55b00cf1afea1ad449e93e4d3f6629a90f
rote1400 <rote1400@student.miun.se>
2024-08-13 11:09:25 +0200

    verification email for successful purchase using Resend

---
+++ src/app/webhooks/stripe/route.tsx
@@ -0,0 +1,57 @@
+import prisma from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const resend = new Resend(process.env.PRIVATE_RESEND_API_KEY as string);
+
+export async function POST(req: NextRequest) {
+  const event = stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+
+  if (event.type === "charge.succeeded") {
+    const charge = event.data.object;
+    const productId = charge.metadata.productId;
+    const email = charge.billing_details.email;
+    const pricePaidInCents = charge.amount;
+
+    const product = await prisma.product.findUnique({
+      where: { id: productId },
+    });
+    if (product == null || email == null) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const userFields = {
+      email,
+      orders: { create: { productId, pricePaidInCents } },
+    };
+
+    const {
+      orders: [order],
+    } = await prisma.user.upsert({
+      where: { email },
+      create: userFields,
+      update: userFields,
+      select: { orders: { orderBy: { createdAt: "desc" }, take: 1 } },
+    });
+
+    const downloadVerification = await prisma.downloadVerification.create({
+      data: {
+        productId,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    });
+
+    resend.emails.send({
+      from: `Support <${process.env.SENDER_EMAIL}>`,
+      to: email,
+      subject: "Order Confirmation",
+      react: <h1>Hi</h1>,
+    });
+  }
+}
