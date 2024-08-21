9ec6ddb0bad0976456fd8e4cb1d01a4e0f3dd065
rote1400 <rote1400@student.miun.se>
2024-08-21 23:44:36 +0200

    order information send via email

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,19 +1,39 @@
-import { Body, Container, Head, Heading, Html, Preview, Tailwind } from "@react-email/components";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
 import { OrderInformation } from "./components/OrderInformation";
 
-export default function PurchaseReceiptEmail( {product}) {
-    return (
-        <Html>
-            <Preview>Download {product.name} and view receipt</Preview>
-            <Tailwind>
-                <Head />
-                <Body className="font-sant bg-white">
-                    <Container className="max-w-xl">
-                        <Heading>Purchase Receipt</Heading>
-                        <OrderInformation />
-                    </Container>
-                </Body>
-            </Tailwind>
-        </Html>
-    )
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+  };
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: { name: "Product name" },
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sant bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 }
--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -1,3 +1,26 @@
-export function OrderInformation() {
-    return <h1>Hi</h1>
+import { Column, Row, Section, Text } from "@react-email/components";
+
+type OrderInformationProps = {
+  order: { id: string };
+  product: {};
+  downloadVerificationId: string;
+};
+
+export function OrderInformation({
+  order,
+  product,
+  downloadVerificationId,
+}: OrderInformationProps) {
+  return (
+    <>
+      <Section>
+        <Row>
+          <Column>
+            <Text>Order ID</Text>
+            <Text>{order.id}</Text>
+          </Column>
+        </Row>
+      </Section>
+    </>
+  );
 }
