51bc56b3446e4d7647098d61a22ed92510e7872c
rote1400 <rote1400@student.miun.se>
2024-10-10 17:20:08 +0200

    purchase receipt

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -13,14 +13,24 @@ type PurchaseReceiptEmailProps = {
   product: {
     name: string;
   };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: String;
 };
 
 PurchaseReceiptEmail.PreviewProps = {
   product: { name: "Product name" },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
 } satisfies PurchaseReceiptEmailProps;
 
 export default function PurchaseReceiptEmail({
   product,
+  order,
+  downloadVerificationId,
 }: PurchaseReceiptEmailProps) {
   return (
     <Html>
@@ -30,7 +40,11 @@ export default function PurchaseReceiptEmail({
         <Body className="font-sant bg-white">
           <Container className="max-w-xl">
             <Heading>Purchase Receipt</Heading>
-            <OrderInformation />
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
           </Container>
         </Body>
       </Tailwind>
--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -1,10 +1,14 @@
 import { Column, Row, Section, Text } from "@react-email/components";
+import { formatCurrency } from "@/lib/formatters";
+import { format } from "path"
 
 type OrderInformationProps = {
-  order: { id: string };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
   product: {};
   downloadVerificationId: string;
 };
+
+const dateFormatter = new Intl.DateTimeFormat("en", {dateStyle: "medium"})
 
 export function OrderInformation({
   order,
@@ -16,8 +20,22 @@ export function OrderInformation({
       <Section>
         <Row>
           <Column>
-            <Text>Order ID</Text>
-            <Text>{order.id}</Text>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Order ID
+            </Text>
+            <Text className="mt-0 mr-4">{order.id}</Text>
+          </Column>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Purchased On
+            </Text>
+            <Text className="mt-0 mr-4">{dateFormatter.format(order.createdAt)}</Text>
+          </Column>
+          <Column>
+            <Text className="mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4">
+              Price Paid
+            </Text>
+            <Text className="mt-0 mr-4">{formatCurrency(order.pricePaidInCents / 100)}</Text>
           </Column>
         </Row>
       </Section>
